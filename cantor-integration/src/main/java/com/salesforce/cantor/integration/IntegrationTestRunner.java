4c8fcfa04ee4c9cfc1c20f13bdf980242823d056
Hnin Yupar Mon <hymonsyep@gmail.com>
2026-08-25 13:44:45 -0700

    Update to only import necessary class

--- cantor-integration/src/main/java/com/salesforce/cantor/integration/IntegrationTestRunner.java
+++ cantor-integration/src/main/java/com/salesforce/cantor/integration/IntegrationTestRunner.java
@@ -12,7 +12,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.TestNG;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class IntegrationTestRunner {
 
